cd20bfde9e10173f6a736f03eb08b5221f2b235b
Antonin <antonin.garret@ens-rennes.fr>
2016-04-06 15:27:33 +0200

    moved setq directive to toplever.cc

--- Code/main.cc
+++ Code/main.cc
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 #include "toplevel.hh"
-=======
 #include <stdio.h>
 #include "object.hh"
 #include "env.hh"
@@ -9,7 +7,6 @@
 extern Object just_read;
 extern "C" int yyparse();
 extern "C" FILE *yyin;
->>>>>>> 9036985e371dc0ab15f1146ff3ded1b8cedce22d
 
 using namespace std;
 
--- Code/toplevel.cc
+++ Code/toplevel.cc
@@ -9,19 +9,26 @@ extern "C" FILE *yyin;
 
 void toplevel()
 {
-  Object a =  symbol_to_Object("a");
+  /*Object a =  symbol_to_Object("a");
   Object b =  symbol_to_Object("b");
   Object one = number_to_Object(1);
   Object two = number_to_Object(2);
 
   Environment env;
   env.add_new_binding(Object_to_string(a), one);
-  env.add_new_binding(Object_to_string(a), two);
-
+  env.add_new_binding(Object_to_string(a), two*/
+  
+  Environment env;
   do {
     cout << "Lisp? " << flush;
     yyparse();
     Object l = just_read;
+    if (Object_to_string(car(l)) == "setq") {
+      Object bound_symbol = cadr(l);
+      Object bound_part = caddr(l);
+      env.add_new_binding(Object_to_string(bound_symbol), eval(bound_part, env));
+      eval(bound_symbol, env);
+    } else
     cout << eval(l, env) << endl;
   } while (!feof(yyin));
 }
