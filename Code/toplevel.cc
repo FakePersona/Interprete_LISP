b4d343b6edfd5f19ddb5178ad92b49d8dc0062b5
Remy Sun <Remy.Sun@ens-rennes.fr>
2016-04-07 11:09:14 +0200

    Made a much cleaner of directive handling. We are raising an exception and catching it when a directive is cast.

--- Code/toplevel.cc
+++ Code/toplevel.cc
@@ -2,34 +2,50 @@
 #include "env.hh"
 #include "eval.hh"
 #include <stdio.h>
+#include <stdexcept>
+
+class Continue_Directive: public runtime_error {
+public:
+  Continue_Directive():
+    runtime_error("Directive error") {
+    
+  }
+};
 
 extern Object just_read;
 extern "C" int yyparse();
 extern "C" FILE *yyin;
 
+void handle_directive(Object l,Environment* env) {
+  if (listp(l) && Object_to_string(car(l)) == "setq") {
+    Object bound_symbol = cadr(l);
+    Object bound_part = caddr(l);
+    env->add_new_binding(Object_to_string(bound_symbol), eval(bound_part, *env));
+    eval(bound_symbol, *env);
+    throw Continue_Directive();
+  }
+}
+
 void toplevel()
 {
-  /*Object a =  symbol_to_Object("a");
+  Object a =  symbol_to_Object("a");
   Object b =  symbol_to_Object("b");
   Object one = number_to_Object(1);
   Object two = number_to_Object(2);
 
   Environment env;
   env.add_new_binding(Object_to_string(a), one);
-  env.add_new_binding(Object_to_string(a), two*/
-  
-  Environment env;
+  env.add_new_binding(Object_to_string(a), two);
+
   do {
     cout << "Lisp? " << flush;
     yyparse();
+    try {
     Object l = just_read;
-    if (Object_to_string(car(l)) == "setq") {
-      Object bound_symbol = cadr(l);
-      Object bound_part = caddr(l);
-      env.add_new_binding(Object_to_string(bound_symbol), eval(bound_part, env));
-      eval(bound_symbol, env);
-    } else
+    handle_directive(l,&env);
     cout << eval(l, env) << endl;
+    } catch (Continue_Directive){
+    }
   } while (!feof(yyin));
 }
 
