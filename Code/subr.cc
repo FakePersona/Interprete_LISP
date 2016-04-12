1e2ff8f46d3a6b16d7cc586a681029f34896aa80
Antonin <antonin.garret@ens-rennes.fr>
2016-04-12 12:52:17 +0200

    corrected + and * subr; working on =

--- Code/subr.cc
+++ Code/subr.cc
@@ -7,14 +7,10 @@
 Not_Subr::Not_Subr():
     runtime_error("Subroutine error") {}
 
-Object handle_subr(Object f,Object lvals){
-	if (Object_to_string(f) == "+") {
-		//return do_plus(lvals);
-	}
-	if (Object_to_string(f) == "*") {
-		//return do_times(lvals);
-	}
-	throw Not_Subr();
+Object do_times(Object lvals) {
+  int a = Object_to_number(car(lvals));
+  int b = Object_to_number(cadr(lvals));
+  return number_to_Object(a * b);
 }
 
 Object do_plus(Object lvals) {
@@ -23,8 +19,27 @@ Object do_plus(Object lvals) {
   return number_to_Object(a + b);
 }
 
-Object do_times(Object lvals) {
-  int a = Object_to_number(car(lvals));
-  int b = Object_to_number(cadr(lvals));
-  return number_to_Object(a * b);
+Object do_eq(Object lvals) {
+	Object a = car(lvals);
+	Object b = cadr(lvals);
+	if (a == b){
+		return number_to_Object(1);
+	}
+	else{
+		return nil();
+	}
 }
+
+Object handle_subr(Object f,Object lvals){
+	if (Object_to_string(f) == "+") {
+		return do_plus(lvals);
+	}
+	if (Object_to_string(f) == "*") {
+		return do_times(lvals);
+	}
+	if (Object_to_string(f) == "=") {
+		return do_eq(lvals);
+	}
+	throw Not_Subr();
+}
+
