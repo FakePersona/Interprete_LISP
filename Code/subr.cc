1584fb53b1883653eede5a392b86f33c074d8ea5
Antonin <antonin.garret@ens-rennes.fr>
2016-04-13 15:14:43 +0200

    added -, car, cdr, cons, reverted some changes on quote

--- Code/subr.cc
+++ Code/subr.cc
@@ -7,16 +7,38 @@
 Not_Subr::Not_Subr():
     runtime_error("Subroutine error") {}
 
+Object do_plus(Object lvals) {
+  int a = Object_to_number(car(lvals));
+  int b = Object_to_number(cadr(lvals));
+  return number_to_Object(a + b);
+}
+
 Object do_times(Object lvals) {
   int a = Object_to_number(car(lvals));
   int b = Object_to_number(cadr(lvals));
   return number_to_Object(a * b);
 }
 
-Object do_plus(Object lvals) {
+Object do_minus(Object lvals) {
   int a = Object_to_number(car(lvals));
   int b = Object_to_number(cadr(lvals));
-  return number_to_Object(a + b);
+  return number_to_Object(a - b);
+}
+
+Object do_car(Object lvals) {
+  Object l = car(lvals);
+  return car(l);
+}
+
+Object do_cdr(Object lvals) {
+  Object l = car(lvals);
+  return cdr(l);
+}
+
+Object do_cons(Object lvals) {
+  Object h = car(lvals);
+  Object t = cadr(lvals);
+  return cons(h, t);
 }
 
 Object do_eq(Object lvals) {
@@ -36,6 +58,18 @@ Object handle_subr(Object f,Object lvals){
 	}
 	if (Object_to_string(f) == "*") {
 		return do_times(lvals);
+	}
+	if (Object_to_string(f) == "-") {
+		return do_minus(lvals);
+	}
+	if (Object_to_string(f) == "car") {
+		return do_car(lvals);
+	}
+	if (Object_to_string(f) == "cdr") {
+		return do_cdr(lvals);
+	}
+	if (Object_to_string(f) == "cons") {
+		return do_cons(lvals);
 	}
 	if (Object_to_string(f) == "=") {
 		return do_eq(lvals);
