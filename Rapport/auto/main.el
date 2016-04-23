c813ba14cd308df35501c4adecf51a9e9c3fd75b
Remy Sun <Remy.Sun@ens-rennes.fr>
2016-04-23 16:57:06 +0200

    Did a bit of work on the rapport

---
+++ Rapport/auto/main.el
@@ -0,0 +1,40 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "a4paper" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("fontenc" "T1") ("inputenc" "utf8") ("babel" "main=francais" "english")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "a4wide"
+    "tabularx"
+    "fullpage"
+    "fontenc"
+    "inputenc"
+    "babel"
+    "graphicx"
+    "xspace"
+    "float"
+    "url"
+    "mathpazo"
+    "listings")
+   (TeX-add-symbols
+    "manual"
+    "dbend"
+    "bfseriesaux")
+   (LaTeX-add-environments
+    "keywords"
+    "remarque"
+    "attention"))
+ :latex)
+
