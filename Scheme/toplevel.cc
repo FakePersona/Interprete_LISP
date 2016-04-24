1d049c581e4687561762637ca4a4bf71dda1413e
Remy Sun <Remy.Sun@ens-rennes.fr>
2016-04-24 16:29:46 +0200

    Code is marginally nicer now

--- Scheme/toplevel.cc
+++ Scheme/toplevel.cc
@@ -55,12 +55,16 @@ void toplevel()
     cout << eval(l, env) << endl;
     }
     catch (Continue_Directive){}
-    /* from this point onwards, it is all goind downhill... */
-    catch (Evaluation_Exception) {}
-    catch (No_Binding_Exception){
-      printf("not bound\n");
+    /* from this point onwards, it is all going downhill... */
+    catch (Evaluation_Exception) {
+      printf("Evaluation exception\n");
     }
-    catch (Zipping_Exception){}
+    catch (No_Binding_Exception){
+      printf("Not bound\n");
+    }
+    catch (Zipping_Exception){
+      printf("Zipping_exception\n");
+    }
     catch (Not_Subr){}
   } while (!end_input());
 }
